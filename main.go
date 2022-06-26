3ebb5bbbeb7b336c39a33ab41e413693c84c80bb
isaaccavallaro <isaaccavallaro@gmail.com>
2022-06-26 15:22:23 +1000

    adding todomodel struct, todo struct and init function

--- main.go
+++ main.go
@@ -20,10 +20,38 @@ import(
 var rnd *renderer.Render
 var db *mgo.Database
 
-const(
+const (
 	hostName 		string = "localhost:27017"
 	dbName 			string = "demo_todo"
 	collectionName 	string = "todo"
 	port 			string = ":9000"
 )
 
+type (
+	todoModel struct{
+		ID		bson.ObjectId`bson:"_id,omitempty"`
+		Title 	string = `bson:"title"`
+		Completed bool `bson:"completed"`
+		CreatedAt time.Time `bson:"createdAt"`
+	}
+	todo struct{
+		ID		string `json:"id"`
+		Title	string `json:"title"`
+		Completed string `json:"completed"`
+		CreatedAt time.Time `json:"created_at"`
+	}	
+)
+
+func init(){
+	rnd = renderer.New()
+	sess, err:=mgo.Dial(hostName)
+	checkErr(err)
+	sess.SetMode(mgo.Monotonic, true)
+	db = sess.DB(dbName)
+}
+
+func checkErr(err error){
+	if err!=nil{
+		log.Fatal(err)
+	}
+}
